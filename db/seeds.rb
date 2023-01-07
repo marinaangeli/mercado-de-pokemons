d40c96e42b79284ec6111480d3d7d74ee0be366f
Marina Angeli <marinasimoes@gmail.com>
2023-01-07 14:19:59 -0300

    final seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # Pokemon.destroy_all
+Deal.destroy_all
 
 # response = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=1500')
 # results = response['results']
@@ -20,14 +21,39 @@
 #   )
 # end
 
-user = User.last
-pokemon = Pokemon.last
+# Date.today.downto(Date.today.since(-60)) do |date|
+#   dates << date.to_s
+# end
 
-Deal.create!(
-  buy_date: Date.yesterday,
-  buy_price: rand(0.1 .. 0.3),
-  sell_date: Date.today,
-  sell_price: rand(0.1 .. 0.3),
-  user: user,
-  pokemon: pokemon
-)
+10.times do
+  user = User.last
+  pokemon = Pokemon.all.sample
+
+  buy_date = Date.today - rand(0..60)
+  date_range = (Date.today - buy_date).to_i
+  days = rand(0..date_range)
+  sell_date = buy_date + days
+
+  Deal.create!(
+    buy_date: buy_date,
+    buy_price: rand(0.1..0.3),
+    sell_date: sell_date,
+    sell_price: rand(0.1..0.3),
+    user: user,
+    pokemon: pokemon
+  )
+end
+
+15.times do
+  user = User.last
+  pokemon = Pokemon.all.sample
+
+  buy_date = Date.today - rand(0..60)
+
+  Deal.create!(
+    buy_date: buy_date,
+    buy_price: rand(0.1..0.3),
+    user: user,
+    pokemon: pokemon
+  )
+end
